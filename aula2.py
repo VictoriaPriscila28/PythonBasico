6e813bdf9a6146644b012c73c602e3d7f3e43798
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-01-10 22:39:19 -0300

    commit inicial

---
+++ aula2.py
@@ -0,0 +1,14 @@
+nome = 'victoria'
+#tipo = type(nome)
+idade = 21
+altura = 1.55
+"""tipo_nome = type(nome)
+tipo_idade = type(idade)
+tipo_altura = type(altura)
+print(tipo_nome)
+print(idade)
+print(tipo_idade)
+print(altura)
+print(tipo_altura)"""
+#concatenação
+print(nome, 'tem', idade, 'anos e', altura, 'de altura')
