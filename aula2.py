885b160031ba0931b6b4105bba3b7347b20e4444
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-01-23 16:05:23 -0300

    aula 2 e 3

--- aula2.py
+++ aula2.py
@@ -1,7 +1,10 @@
-nome = 'victoria'
+#nome = 'victoria'
 #tipo = type(nome)
-idade = 21
-altura = 1.55
+'''idade = 22
+altura = 1.58
+tipo_nome = type(nome)
+tipo_idade = type(idade)'''
+
 """tipo_nome = type(nome)
 tipo_idade = type(idade)
 tipo_altura = type(altura)
@@ -11,4 +14,18 @@ print(tipo_idade)
 print(altura)
 print(tipo_altura)"""
 #concatenação
-print(nome, 'tem', idade, 'anos e', altura, 'de altura')
+'''print(nome, 'tem', idade, 'anos e', altura, 'de altura')
+print(tipo_nome)
+print(tipo_idade)
+
+nome2 = input('Digite seu nome: ')
+print(nome2, 'tem', idade, 'anos e', altura, 'de altura')'''
+print('---------------------------------------------------------------')
+print("Para prosseguir digite seus dados: ")
+nome = input('Digite seu nome: ')
+idade = input('digite sua idade: ')
+cpf = input('Digite seu cpf: ')
+altura = float(input('digite sua altura: '))
+telefone = input('Digite seu numero de telefone: ')
+endereco = input('Digite seu endereço: ')
+print(nome, idade, cpf, altura, telefone, endereco)
---
+++ aula3.py
@@ -0,0 +1,27 @@
+#operadores logicos
+#tabela verdade
+'''a = 50
+b = 20
+if a > b and 'abacaxi' == 'uva':
+    print(a, 'é maior que', b)
+else:
+    print(a, 'nao é maior que', b)
+
+print(not True)
+print(not False)
+idade = 50
+if not idade == 50:
+    print('voce nao tem 50 anos')
+else:
+    print('voce tem 50 anos')'''
+
+print('exercicio')
+peso = float(input('digite seu peso: '))
+idade = int(input('digite sua idade: '))
+altura = float(input('digite sua altura:  '))
+
+if peso >= 60 and idade > 18 and altura >= 1.70:
+    print('voce esta apto a entrar no exercito')
+else:
+    print('voce nao vai entrar no exercito')
+
