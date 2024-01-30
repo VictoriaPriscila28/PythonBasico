4e80dad62236d30ee45e473370692a1356311445
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-01-30 13:06:47 -0300

    resolucao do exercio 2

--- aula2.py
+++ aula2.py
@@ -20,6 +20,11 @@ print(tipo_idade)
 
 nome2 = input('Digite seu nome: ')
 print(nome2, 'tem', idade, 'anos e', altura, 'de altura')'''
+
+''' Exercicio
+Faça um formulario que pergunte: nome, cpf, endereço, idade, altura e telefone.
+E imprima isso em um relatorio organizado.'''
+
 print('---------------------------------------------------------------')
 print("Para prosseguir digite seus dados: ")
 nome = input('Digite seu nome: ')
@@ -28,4 +33,14 @@ cpf = input('Digite seu cpf: ')
 altura = float(input('digite sua altura: '))
 telefone = input('Digite seu numero de telefone: ')
 endereco = input('Digite seu endereço: ')
-print(nome, idade, cpf, altura, telefone, endereco)
+
+print('Leia com atenção se suas informações estão corretas: ')
+print('Seu nome é',nome,',voce tem', idade,'anos e', altura, 'metros de altura. \n' 
+'Seu CPF é:', cpf,'.', 'Seu telefone de contato é: ', telefone, ',e por fim, seu endereço é:', endereco, '.')
+
+res = str(input('Esta tudo certo?')).upper().strip()
+
+if res == 'sim':
+    print('entao tudo certo')
+else:
+    print('por favor refaça seu cadastro')
