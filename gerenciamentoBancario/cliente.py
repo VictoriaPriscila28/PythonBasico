526323cc665e4ac06120fda8b0810ed165038b39
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-02-03 14:30:21 -0300

    resolucao exe 9

--- gerenciamentoBancario/cliente.py
+++ gerenciamentoBancario/cliente.py
@@ -5,3 +5,10 @@ class Cliente:
         self.nome = nome
         self.cpf = cpf
         self.idade = idade
+
+'''essa foi a alteração que o Guilherme fez, no caso ao colocar str
+o retorno já vem com as indicações corretas, sem a necessidade de adicionar
+isso no main.Nesse caso quando for utilizar isso no main apenas colocaria print(cliente1)
+     def __str__(self):
+        return "Nome: " + self.nome + "\nCPF: " + self.cpf + "\nIdade: " + str(self.idade)
+        '''
