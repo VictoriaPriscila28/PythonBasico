6b27336b8b39ef68a229a6c72d70b6c85bab3933
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-02-09 10:38:53 -0300

    add part 2 aula 14

--- Aulas/aula14.py
+++ Aulas/aula14.py
@@ -1,12 +1,15 @@
 # expressoẽs regulares
 ''' mini liguagem que te ajuda a identificar padroes'''
 import re
-string_de_teste = 'o gatos é bonito'
-padrao = re.search(r'gat\w\w',string_de_teste) # raw string
-print(padrao.group())
+import requests
+#string_de_teste = 'jffnjfk@fnkdf.com'
+#padrao = re.search(r'gat\w+',string_de_teste) # raw string
+requisicao = requests.get('https://datametricapesquisas.com.br/fale-conosco/')
+padrao = re.findall(r'[\w\.-]+@[\w-]+\.[\w\.-]+',requisicao.text)
+#print(padrao.group())
 #print(r'oi pessoal\n segunda linha')
-
+#https://regex101.com/
 if padrao:
-    print(padrao.group())
+    print(padrao)
 else:
     print('padrao nao encontrado')
